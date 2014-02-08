8733884ff85665a69af1bf8edf23d8534fb03d08
yuhong <yikten@163.com>
2014-02-08 17:49:14 +0800

    20140208
    20140208

---
+++ Android/TwoActivity/src/com/example/twoactivity/OtherAcitvity.java
@@ -0,0 +1,17 @@
+package com.example.twoactivity;
+
+import android.R.layout;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OtherAcitvity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_two);
+	}
+
+}
